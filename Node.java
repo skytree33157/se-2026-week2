6304680376a0f53ddeb86bb6a29b6aa0e3f9034b
njm0333_desk <shwjdals321@gmail.com>
2026-09-09 20:56:04 +0900

    add hasChild,dfs

--- Node.java
+++ Node.java
@@ -14,4 +14,15 @@ public class Node {
     public void addChild(Node child) {
         this.children.add(child);
     }
+
+
+    public boolean hasChild() { // Check if the node has any children and return true if it does, false otherwise
+        return this.children != null && !this.children.isEmpty();
+    }
+
+    public void dfs() {
+        System.out.println(this.label);
+        if (!this.hasChild()) return;
+        for (Node child : this.children) if (child != null) child.dfs();
+    }
 }

9f3df308377a1fd3bd57c1f055f72aa693c2df7d
skytree33157 <handaewun@gmail.com>
2026-09-10 18:43:39 +0900

    height 구현

--- Node.java
+++ Node.java
@@ -14,4 +14,18 @@ public class Node {
     public void addChild(Node child) {
         this.children.add(child);
     }
+
+    public int height(){
+        if(children.isEmpty()){
+            return 0;
+        }
+        int maxHeight=0;
+        for(Node child: children){
+            int childHeight=child.height();
+            if(childHeight>maxHeight){
+                maxHeight=childHeight;
+            }
+        }
+        return maxHeight+1;
+    }
 }
